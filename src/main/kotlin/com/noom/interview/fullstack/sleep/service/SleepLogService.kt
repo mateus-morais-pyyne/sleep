e4cc63852ac504fa879beeba924a1dea19a41112
Mateus Morais <mateus.morais@pyyne.com>
2025-06-22 13:23:39 -0300

    Task #3 - Fixed minor bugs. Created Util class.

--- src/main/kotlin/com/noom/interview/fullstack/sleep/service/SleepLogService.kt
+++ src/main/kotlin/com/noom/interview/fullstack/sleep/service/SleepLogService.kt
@@ -1,15 +1,14 @@
 package com.noom.interview.fullstack.sleep.service
 
+import com.noom.interview.fullstack.sleep.commons.Util
 import com.noom.interview.fullstack.sleep.db.SleepLogRepository
 import com.noom.interview.fullstack.sleep.model.SleepLog
 import com.noom.interview.fullstack.sleep.model.dto.CreateSleepLogDTO
-import com.noom.interview.fullstack.sleep.model.enumeration.MorningFeeling
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.server.ResponseStatusException
-import java.time.temporal.ChronoUnit
 import java.util.*
 
 @Service
@@ -21,25 +20,15 @@ class SleepLogService(
     @Transactional
     fun createSleepLog(createSleepLogDTO: CreateSleepLogDTO, userId: UUID): SleepLog {
         val user = userService.findById(userId)
-        
-        val totalTimeInBed = ChronoUnit.MINUTES.between(
-            createSleepLogDTO.bedTime,
-            createSleepLogDTO.wakeUpTime
-        ).toInt()
-        
-        if (totalTimeInBed < 0) {
-            throw ResponseStatusException(
-                HttpStatus.BAD_REQUEST,
-                "Wake up time must be after bed time"
-            )
-        }
-        
+
+        val totalTimeInBed = Util.getTotalTimeInBed(createSleepLogDTO)
+
         val sleepLog = SleepLog(
             dateOfSleep = createSleepLogDTO.bedTime.toLocalDate(),
             bedTime = createSleepLogDTO.bedTime,
             wakeUpTime = createSleepLogDTO.wakeUpTime,
             totalTimeInBed = totalTimeInBed,
-            morningFeeling = MorningFeeling.valueOf(createSleepLogDTO.morningFeeling.uppercase()),
+            morningFeeling = createSleepLogDTO.morningFeeling,
             user = user
         )
         
